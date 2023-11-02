308df55ef2867eb5a60af7703c6c7d56a3e5ac57
John Karmozyn <hughkarmozyn@icloud.com>
2023-11-02 11:45:58 -0600

    Added Stack and StackNode class

---
+++ PostfixCalculator/src/Stack.java
@@ -0,0 +1,53 @@
+public class Stack <T> {
+
+    private int size; // size
+    private StackNode top; // top element
+
+
+
+    Stack(){ // default constructer
+
+    }
+
+
+
+    public void push(T data){ // push method
+        size++; // adds to size
+        StackNode n = new StackNode(data, top);
+        top=n; // sets top to add element
+
+
+
+    }
+
+
+    public  T pop(){ // pop method
+        size--; // subtracts from the 1 from the size
+        StackNode oldTop = top;
+        top=top.getParent(); // sets the new top
+        return (T)oldTop.getData(); // returns the deleted element
+
+
+
+
+    }
+    public T peek(){
+        return (T)top.getData();
+    } // peek method returns the element
+    public int getSize(){ // returns the size
+        return size;
+
+    }
+
+    public boolean isEmpty(){ // is empty
+        if(size==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+
+
+}
---
+++ PostfixCalculator/src/StackNode.java
@@ -0,0 +1,50 @@
+public class StackNode<T> {
+
+    private T data; //Data stored in the node
+    private StackNode parent; //Reference to the parent node
+
+    //Default constructor
+    public StackNode() {
+
+    }
+
+    //Constructor that sets data of the node
+    public StackNode(T data) {
+        this.data = data;
+    }
+
+    //Constructor that sets data, parent, and child
+    public StackNode(T data, StackNode parent) {
+        this.data = data;
+        this.parent = parent;
+    }
+
+    //Returns the data stored in the node
+    public T getData() {
+        return data;
+    }
+
+    //Sets the data for this node
+    public void setData(T data) {
+        this.data = data;
+    }
+
+
+    public StackNode getParent() {
+        return parent;
+    }
+
+    public void setParent(StackNode n) {
+        parent = n;
+    }
+
+    //Provides a string representation of the node by converting its data to a string
+    public String toString() {
+        return "StackNode: " + this.data.toString();
+    }
+
+    //Compares the data of the node with the data of another node 'n'
+    public boolean equals(StackNode n) {
+        return this.data.equals(n.getData());
+    }
+}
